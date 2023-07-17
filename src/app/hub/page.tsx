7c3851ab3a4c78ae36649c1867dcf7ff7db15c6b
Jax <me@calebdelbridge.com>
2023-07-17 23:42:17 +0300

    feat: hub layout

--- src/app/hub/page.tsx
+++ src/app/hub/page.tsx
@@ -18,14 +18,14 @@ const MePage = async () => {
   return (
     <div className="flex-1 space-y-4 p-8 pt-6">
       <div className="flex items-center justify-between space-y-2">
-        <h2 className="text-3xl font-bold tracking-tight">My Tournaments</h2>
+        <h2 className="text-3xl font-bold tracking-tight">Tournament Hub</h2>
         <div className="flex items-center space-x-2">
           <Link href="/tournaments">
-            <Button>View Upcoming Tournaments</Button>
+            <Button>View All Tournaments</Button>
           </Link>
         </div>
       </div>
-      <div className="space-y-4">
+      <div className="space-y-8">
         <div className="grid grid-cols-3 gap-4">
           <StatisticCard title="Available Credits" value={statistics.credits} />
           <StatisticCard
@@ -37,19 +37,24 @@ const MePage = async () => {
             value={statistics.wonTournaments}
           />
         </div>
-        <div className="grid grid-cols-1 lg:grid-cols-3 gap-4">
-          {tournaments
-            .filter((tournament) =>
-              tournament.participants.find(
-                (participant) => participant.user.id === session.user.id
+        <div className="space-y-4">
+          <h2 className="text-2xl font-semibold tracking-tight">
+            Upcoming Tournaments
+          </h2>
+          <div className="grid grid-cols-1 lg:grid-cols-3 gap-4">
+            {tournaments
+              .filter((tournament) =>
+                tournament.participants.find(
+                  (participant) => participant.user.id === session.user.id
+                )
               )
-            )
-            .map((tournament) => (
-              <ExperienceTournamentCard
-                key={tournament.id}
-                tournament={tournament}
-              />
-            ))}
+              .map((tournament) => (
+                <ExperienceTournamentCard
+                  key={tournament.id}
+                  tournament={tournament}
+                />
+              ))}
+          </div>
         </div>
       </div>
     </div>
