a0bea4cdc2b3fdf4baa33130b4556a9d17f4ced7
Jax <me@calebdelbridge.com>
2023-07-21 01:59:01 +0300

    feat: add sentry config

--- next.config.js
+++ next.config.js
@@ -1,9 +1,25 @@
+const { withSentryConfig } = require("@sentry/nextjs");
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    experimental: {
-        serverActions: true,
-        appDir: true
-    }
+  experimental: {
+    serverActions: true,
+    appDir: true
+  }
 }
 
-module.exports = nextConfig
+module.exports = withSentryConfig(
+  nextConfig,
+  {
+    silent: true,
+    org: "sulani",
+    project: "tourney",
+  },
+  {
+    widenClientFileUpload: true,
+    transpileClientSDK: true,
+    tunnelRoute: "/monitoring",
+    hideSourceMaps: true,
+    disableLogger: true,
+  }
+);
