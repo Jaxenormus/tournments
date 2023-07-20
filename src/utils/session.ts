4777b28a4e83ca908712177768148b60b682c4d9
Jax <me@calebdelbridge.com>
2023-07-20 06:53:12 +0300

    add additional logs

--- src/utils/session.ts
+++ src/utils/session.ts
@@ -24,6 +24,7 @@ export const isExperienceOwner = async (
       }
     );
     const data = (await res.json()) as HasAccessResponse;
+    console.log(data);
     return data.authorized_user?.permission_level === 0;
   } catch (e) {
     return false;
@@ -41,6 +42,7 @@ export const hasAccess = async (
   const userId = cookies().get("tourney_uid");
   const experienceId = cookies().get("tourney_eid");
   const accessToken = cookies().get("tourney_at");
+  console.log(userId, experienceId, accessToken);
   if (!userId || !experienceId || !accessToken) {
     return redirect("/no-access");
   }
@@ -48,6 +50,7 @@ export const hasAccess = async (
     where: { id: userId.value },
     include: { memberships: true },
   });
+  console.log(user);
   if (!user) {
     return redirect("/no-access");
   }
@@ -61,6 +64,7 @@ export const hasAccess = async (
   if (access === "admin" || access === "adminOrConsumer") {
     isAdmin = await isExperienceOwner(accessToken.value, experienceId.value);
   }
+  console.log(isAdmin, hasMembership);
   if (
     (access === "adminOrConsumer" && !isAdmin && !hasMembership) ||
     (access === "admin" && !isAdmin) ||
